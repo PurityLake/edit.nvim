37a7c652e0621be71b24f081f158521f7517bc9f
Robert O'Shea <RobertOShea2k11@gmail.com>
2023-12-23 16:11:10 +0000

    Added a two delimiter delete function

--- lua/edit-nvim/delimiter_delete.lua
+++ lua/edit-nvim/delimiter_delete.lua
@@ -1,6 +1,6 @@
 local M = {}
 
-function M.delimiter_delete(delim)
+function M.delimiter_one_delete(delim)
 	local current_line = vim.api.nvim_get_current_line()
 	local row, col = unpack(vim.api.nvim_win_get_cursor(0))
 	local new_line = ""
@@ -35,8 +35,47 @@ function M.delimiter_delete(delim)
 
 	new_line = current_line:sub(1, start_idx + delim:len() - 1) .. current_line:sub(end_idx, current_line:len())
 
-	vim.api.nvim_buf_set_lines(0, row - 1, row, true, { new_line })
 	vim.api.nvim_win_set_cursor(0, { row, start_idx + delim:len() - 1 })
+end
+
+function M.delimiter_two_delete(delim1, delim2)
+	local current_line = vim.api.nvim_get_current_line()
+	local row, col = unpack(vim.api.nvim_win_get_cursor(0))
+	local new_line = ""
+
+	local start_idx = -1
+	local end_idx = -1
+
+	for i = col, 1, -1 do
+		local str = current_line:sub(i, i + delim1:len() - 1)
+		if str == delim1 then
+			start_idx = i
+			break
+		end
+	end
+
+	for i = col + 1, current_line:len(), 1 do
+		local str = current_line:sub(i, i + delim2:len() - 1)
+		if str == delim2 then
+			end_idx = i
+			break
+		end
+	end
+
+	if start_idx == -1 or end_idx == -1 then
+		print("No matching delimiter found")
+		return
+	end
+
+	if start_idx == end_idx then
+		return
+	end
+
+	new_line = current_line:sub(1, start_idx + delim1:len() - 1)
+		.. current_line:sub(end_idx + delim2:len() - 1, current_line:len())
+
+	vim.api.nvim_buf_set_lines(0, row - 1, row, true, { new_line })
+	vim.api.nvim_win_set_cursor(0, { row, start_idx + delim1:len() - 1 })
 end
 
 return M
--- lua/edit-nvim/init.lua
+++ lua/edit-nvim/init.lua
@@ -2,23 +2,44 @@ local delim_delete = require("edit-nvim.delimiter_delete")
 
 local M = {}
 
-M.delimiter_delete = delim_delete.delimiter_delete
+M.delimiter_one_delete = delim_delete.delimiter_one_delete
+M.delimiter_two_delete = delim_delete.delimiter_two_delete
 
 local default_opts = {
-    delimiter_delete = "dd",
+	delimiter_one_delete_key = "<leader>d1",
+	delimiter_two_delete_key = "<leader>d2",
 }
 
 function M.setup(opts)
-    opts = opts or default_opts
-    vim.keymap.set("n", "<leader>" .. opts.delimiter_delete, function()
-        local input = vim.fn.input("Delimiter: ")
-        if input == "" then
-            print("No delimiter provided")
-            return
-        end
-        require("edit-nvim").delimiter_delete(input)
-    end)
+	opts = opts or {}
+	for k, v in pairs(default_opts) do
+		if not opts[k] then
+			opts[k] = v
+		end
+	end
+
+	vim.keymap.set("n", opts.delimiter_one_delete_key, function()
+		local input = vim.fn.input("Delimiter: ")
+		if input == "" then
+			print("No delimiter provided")
+			return
+		end
+		require("edit-nvim").delimiter_one_delete(input)
+	end, { desc = "Delete text between a single provided delimiter" })
+
+	vim.keymap.set("n", opts.delimiter_two_delete_key, function()
+		local input1 = vim.fn.input("Left Delimiter: ")
+		if input1 == "" then
+			print("No delimiter provided")
+			return
+		end
+		local input2 = vim.fn.input("Right Delimiter: ")
+		if input2 == "" then
+			print("No delimiter provided")
+			return
+		end
+		require("edit-nvim").delimiter_two_delete(input1, input2)
+	end, { desc = "Delete text between two different delimiters" })
 end
 
 return M
-
