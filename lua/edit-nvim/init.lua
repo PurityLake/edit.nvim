7dfb36f5c9cbda759c25d7ce93aa0ce88ada326e
Robert O'Shea <robertoshea2k11@gmail.com>
2023-12-23 03:18:34 +0000

    Added way to setup project

--- lua/edit-nvim/init.lua
+++ lua/edit-nvim/init.lua
@@ -4,5 +4,21 @@ local M = {}
 
 M.delimiter_delete = delim_delete.delimiter_delete
 
+local default_opts = {
+    delimiter_delete = "dd",
+}
+
+function M.setup(opts)
+    opts = opts or default_opts
+    vim.keymap.set("n", "<leader>" .. opts.delimiter_delete, function()
+        local input = vim.fn.input("Delimiter: ")
+        if input == "" then
+            print("No delimiter provided")
+            return
+        end
+        require("edit-nvim").delimiter_delete(input)
+    end)
+end
+
 return M
 
